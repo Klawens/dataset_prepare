5ed9d0e8701e2e513ebb2e41f98b57f08d3b8e43
Shichang Liu <klawens@memtrail.com>
2021-01-06 16:48:09 +0800

    Create image2csv.py

---
+++ image2csv.py
@@ -0,0 +1,14 @@
+import os, csv
+
+
+f = open('test.csv', 'w')
+images = os.listdir('/4TB-HDD1/lsc/data/kaggle/xray_det/test_jpg/')
+csv_writer = csv.writer(f)
+
+for i in images:
+    # print(i)
+    name = os.path.splitext(i)[0]
+    csv_writer.writerow([name, 1, 1, 1, 1, "ILD"])
+    print(name)
+
+f.close()
