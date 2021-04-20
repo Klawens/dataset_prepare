32b7add0fa8d585e52fac6f9eed1e80f5d2e49b4
Shichang Liu <klawens@memtrail.com>
2021-04-20 10:04:25 +0800

    Update wbf_for_coco_json_result.py

--- wbf_for_coco_json_result.py
+++ wbf_for_coco_json_result.py
@@ -5,6 +5,10 @@ import json, os, numpy, cv2
 model0 = json.load(open('1.bbox.json', 'r'))
 model1 = json.load(open('2.bbox.json', 'r'))
 
+weights = [2, 1]
+iou_thr = 0.5
+skip_box_thr = 0.0001
+
 n = 0
 for img in os.listdir('data/test/'):
     w, h = cv2.imread('data/test/'+img).shape[1], cv2.imread('data/test/'+img).shape[0]
@@ -31,19 +35,15 @@ for img in os.listdir('data/test/'):
             model1_label.append(img1['category_id'])
             model1_id.append(img1['image_id'])
 
-
     boxes_list = [model0_box, model1_box]
     scores_list = [model0_score, model1_score]
     labels_list = [model0_label, model1_label]
-    weights = [2, 1]
-
-    iou_thr = 0.5
-    skip_box_thr = 0.0001
 
     boxes, scores, labels = weighted_boxes_fusion(
         boxes_list, scores_list, labels_list, weights=weights, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
     # boxes_list = numpy.array(boxes_list)
     # print(boxes.shape)
+    
 
     ######### Your output format here #########
     # output = open('res/{}.txt'.format(img.split('.')[0]), 'w')
