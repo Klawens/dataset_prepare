d6b4654307bec599b466a17552cceadc2c83b1b3
Shichang Liu <klawens@memtrail.com>
2021-05-18 16:40:04 +0800

    Update dicom2jpg.py

--- dicom2jpg.py
+++ dicom2jpg.py
@@ -2,13 +2,13 @@ import pydicom
 import scipy.misc
 import os
 
-path = '/4TB-HDD1/lsc/data/kaggle/xray_det/test/'
+path = 'dicom_path'
 images = os.listdir(path)
 #print(images)
 for i in images:
     #print(i)
     in_ = path + i
-    out = '/4TB-HDD1/lsc/data/kaggle/xray_det/test_jpg/' + os.path.splitext(i)[0] + '.jpg'
+    out = 'jpg_path' + os.path.splitext(i)[0] + '.jpg'
     ds = pydicom.read_file(in_)
     img = ds.pixel_array
     print(img.shape)
