0db63fab3246f56ce64eacaf19541fcbdac8b1a7
Shichang Liu <klawens@memtrail.com>
2021-09-01 22:42:28 +0800

    Update xml2csv.py

--- xml2csv.py
+++ xml2csv.py
@@ -7,30 +7,46 @@ import os
 import xml.dom.minidom as md
 from tqdm import tqdm
 
-xmls = '../trainData/gt/'
-res = csv.writer(open('./train.csv', 'w', newline=''))
+xmls = 'val/'
+res = csv.writer(open('./val.csv', 'w', newline=''))
 unique_cat = []
 
 for xml in tqdm(os.listdir(xmls)):
     DOMTree = md.parse(xmls + xml)
     collection = DOMTree.documentElement
     categories = collection.getElementsByTagName('name')
-    boxes = collection.getElementsByTagName('point')  # Change this field to VOC tag
+    # boxes = collection.getElementsByTagName('point')  # Change this field to VOC tag
+    xmin = collection.getElementsByTagName('xmin')
+    ymin = collection.getElementsByTagName('ymin')
+    xmax = collection.getElementsByTagName('xmax')
+    ymax = collection.getElementsByTagName('ymax')
 
     category_list = []
-    box_list = []
+    xmin_list = []
+    ymin_list = []
+    xmax_list = []
+    ymax_list = []
     for cat in categories:
         category_list.append(cat.childNodes[0].data)
         if cat.childNodes[0].data not in unique_cat:
             unique_cat.append(cat.childNodes[0].data)
-    for box in boxes:
-        box_list.append(box.childNodes[0].data.split(','))
+    # for box in boxes:
+    #     box_list.append(box.childNodes[0].data.split(','))
+    for x_min in xmin:
+        xmin_list.append(x_min.childNodes[0].data)
+    for y_min in ymin:
+        ymin_list.append(y_min.childNodes[0].data)
+    for x_max in xmax:
+        xmax_list.append(x_max.childNodes[0].data)
+    for y_max in ymax:
+        ymax_list.append(y_max.childNodes[0].data)
 
     n = 0
-    step = 4  # 4 if is voc format
+    step = 1  # 1 if is voc format
     for category in category_list:
         # modify this if is voc format
-        res.writerow([xml.split('.')[0], box_list[0], box_list[1], box_list[2], box_list[3], category])
+        # if category != 'waterweeds':
+        res.writerow([xml.split('.')[0], xmin_list[n], ymin_list[n], xmax_list[n], ymax_list[n], category])
         # res.writerow(
         #     [xml.split('.')[0], box_list[n][0], box_list[n][1], box_list[n + 1][0], box_list[n + 2][1], category])
         n += step
